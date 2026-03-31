7076d1692fb2f7339e1eb9777832432f156f3c51
SUBATHRA394 <144546731+SUBATHRA394@users.noreply.github.com>
2026-03-31 10:20:35 +0530

    Create trapping rain.js

---
+++ java/trapping rain.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = 0;
+    let rightMax = 0;
+    let totalWater = 0;
+
+    while (left < right) {
+        // We always process the side with the shorter height
+        if (height[left] < height[right]) {
+            // Update left maximum or calculate trapped water
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                totalWater += leftMax - height[left];
+            }
+            left++;
+        } else {
+            // Update right maximum or calculate trapped water
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                totalWater += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+
+    return totalWater;
+};
+
+// --- Test Cases ---
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])); // Output: 6
+console.log(trap([4,2,0,3,2,5]));             // Output: 9
