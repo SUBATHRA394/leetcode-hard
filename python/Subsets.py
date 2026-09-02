41723730662c068997452254b7c25e437c616d58
SUBATHRA394 <144546731+SUBATHRA394@users.noreply.github.com>
2026-09-02 10:36:50 +0530

    Create Subsets.py

---
+++ python/Subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsetsWithDup(self, nums):
+        nums.sort()
+        result = []
+        
+        def backtrack(start, current):
+            result.append(list(current))
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                current.append(nums[i])
+                backtrack(i + 1, current)
+                current.pop()
+                
+        backtrack(0, [])
+        return result
